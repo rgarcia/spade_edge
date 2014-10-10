1210b96e1af823939e46159de6e9b1a2e2307ce7
Kai Hayashi <kai.hayashi@twitch.tv>
2014-10-10 13:59:15 -0700

    tweaked some settings

--- build/finalize.sh
+++ build/finalize.sh
@@ -11,5 +11,11 @@ PKGDIR="/tmp/pkg"
 mv ${PKGDIR}/deploy ${EDGEDIR}
 chmod +x ${EDGEDIR}/bin/*
 
+sudo apt-get update 
+sudo apt-get install -y dpkg
+
+curl -O http://science.twitch.tv/debs/kafka_finder_1.0.3_amd64.deb
+sudo dpkg -i kafka_finder_1.0.3_amd64.deb
+
 # Setup upstart
 mv ${CONFDIR}/upstart.conf ${UPSTARTDIR}/spade_edge.conf
